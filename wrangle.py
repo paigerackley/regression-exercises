1c2810a55cd09bd8f2651f6b72b14a3cb1dd0b33
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-05-31 21:24:56 -0500

    Finished wrangle exercise

--- wrangle.py
+++ wrangle.py
@@ -2,44 +2,78 @@ import pandas as pd
 import os
 from env import get_db_url
 
-###### ACQUIRE ###########
-
+"""
+USAGE: 
+Use `from wrangle import wrangle_zillow` at the top of your notebook.
+This 
+"""
 def get_zillow_data():
-    filename = 'zillow_data.csv'
+    """Seeks to read the cached zillow.csv first """
+    filename = "zillow.csv"
 
     if os.path.isfile(filename):
-        return pd.read_csv(filename, index_col=0)
+        return pd.read_csv(filename)
     else:
-        df = pd.read_sql(
-            '''
-            SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 WHERE propertylandusetypeid = 261; 
-            '''
-            ,
-            get_db_url('zillow')
-        )
+        return get_new_zillow_data()
 
-        df.to_csv(filename)
+def get_new_zillow_data():
+    """Returns a dataframe of all 2017 properties that are Single Family Residential"""
 
-        return df
-######### PREPARE #############
+    sql = """
+    select 
+    bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    from properties_2017
+    join propertylandusetype using (propertylandusetypeid)
+    where propertylandusedesc = "Single Family Residential"
+    """
+    return pd.read_sql(sql, get_db_url("zillow"))
+
+
+def handle_nulls(df):    
+    # We keep 99.41% of the data after dropping nulls
+    # round(df.dropna().shape[0] / df.shape[0], 4) returned .9941
+    df = df.dropna()
+    return df
+
+
+def optimize_types(df):
+    # Convert some columns to integers
+    # fips, yearbuilt, and bedrooms can be integers
+    df["fips"] = df["fips"].astype(int)
+    df["yearbuilt"] = df["yearbuilt"].astype(int)
+    df["bedroomcnt"] = df["bedroomcnt"].astype(int)    
+    df["taxvaluedollarcnt"] = df["taxvaluedollarcnt"].astype(int)
+    df["calculatedfinishedsquarefeet"] = df["calculatedfinishedsquarefeet"].astype(int)
+    return df
+
+
+def handle_outliers(df):
+    """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
+    df = df[df.bathroomcnt <= 6]
+    
+    df = df[df.bedroomcnt <= 6]
+
+    df = df[df.taxvaluedollarcnt < 2_000_000]
+
+    return df
 
 
 def wrangle_zillow():
-    '''
-    Acquires and prepares zillow data for exploration and modeling
-    '''
-    # Pull data using an acquire function
+    """
+    Acquires Zillow data
+    Handles nulls
+    optimizes or fixes data types
+    handles outliers w/ manual logic
+    returns a clean dataframe
+    """
     df = get_zillow_data()
 
-    # Drop all nulls from dataset
-    df = df.dropna()
+    df = handle_nulls(df)
 
-    # Convert to integers where we can
-    df = df.astype({'bedroomcnt':'int', 'calculatedfinishedsquarefeet':'int', 'taxvaluedollarcnt':'int', 'yearbuilt':'int', 'taxamount':'int','fips':'int'})
+    df = optimize_types(df)
 
-    # Eliminate the funky values
-    df = df[df['calculatedfinishedsquarefeet'] > 400]
-    df = df[df['taxvaluedollarcnt'] > 10000]
-    df = df[df['taxamount'] > 200]
+    df = handle_outliers(df)
+
+    df.to_csv("zillow.csv", index=False)
 
     return df
