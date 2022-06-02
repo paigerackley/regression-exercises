a112b8c5d1b0788d8da92bfad86d3f8fc397c5ee
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-02 14:15:17 -0500

    Updating

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from env import get_db_url
+#### ACQUIRE ######
 
 """
 USAGE: 
@@ -28,6 +29,11 @@ def get_new_zillow_data():
     """
     return pd.read_sql(sql, get_db_url("zillow"))
 
+    ###########################
+
+
+    #### Functions inside of wrangle ############
+
 
 def handle_nulls(df):    
     # We keep 99.41% of the data after dropping nulls
@@ -44,18 +50,29 @@ def optimize_types(df):
     df["bedroomcnt"] = df["bedroomcnt"].astype(int)    
     df["taxvaluedollarcnt"] = df["taxvaluedollarcnt"].astype(int)
     df["calculatedfinishedsquarefeet"] = df["calculatedfinishedsquarefeet"].astype(int)
+
+    
+    # readability
+    df = df.rename(columns={'calculatedfinishedsquarefeet': 'sqr_ft'})
     return df
 
+    
 
 def handle_outliers(df):
-    """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
-    df = df[df.bathroomcnt <= 6]
-    
-    df = df[df.bedroomcnt <= 6]
-
-    df = df[df.taxvaluedollarcnt < 2_000_000]
+     # Eliminate the funky values
+    df = df[df['calculatedfinishedsquarefeet'] > 400]
+    df = df[df['calculatedfinishedsquarefeet'] < 100000]
+    df = df[df['taxvaluedollarcnt'] > 10000]
+    df = df[df['taxvaluedollarcnt'] < 20000000]
+    df = df[df['taxamount'] > 100]
+    df = df[df['taxamount'] < 300000]
+    df = df[df['bathroomcnt'] > 0]
+    df = df[df['bedroomcnt'] > 0]
+    df = df[df['bathroomcnt'] < 7]
+    df = df[df['bedroomcnt'] < 7]
 
     return df
+
 
 
 def wrangle_zillow():
@@ -77,3 +94,18 @@ def wrangle_zillow():
     df.to_csv("zillow.csv", index=False)
 
     return df
+
+### SPLIT #####
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable, and an integer for a setting a seed
+    and splits the data into train, validate and test.
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the
+    original dataset, and train is .70*.80= 56% of the original dataset.
+    The function returns, in this order, train, validate and test dataframes.
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2,
+                                            random_state=seed)
+    train, validate = train_test_split(train_validate, test_size=0.3,
+                                       random_state=seed)
+    return train, validate, test
