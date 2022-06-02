daa3ad26bb3c88abe201e01d6572cc6e48115f3b
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-02 15:22:45 -0500

    fadsfdsf

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from env import get_db_url
+
 #### ACQUIRE ######
 
 """
@@ -30,6 +31,56 @@ def get_new_zillow_data():
     return pd.read_sql(sql, get_db_url("zillow"))
 
     ###########################
+
+
+### SPLIT #####
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable, and an integer for a setting a seed
+    and splits the data into train, validate and test.
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the
+    original dataset, and train is .70*.80= 56% of the original dataset.
+    The function returns, in this order, train, validate and test dataframes.
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2,
+                                            random_state=seed)
+    train, validate = train_test_split(train_validate, test_size=0.3,
+                                       random_state=seed)
+    return train, validate, test
+
+
+#### Scale #####
+def scale_data(train, 
+               validate, 
+               test, 
+               columns_to_scale=['bedroomcnt', 'bathroomcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet'],
+               return_scaler=False):
+    '''
+    Scales the 3 data splits. 
+    Takes in train, validate, and test data splits and returns their scaled counterparts.
+    If return_scalar is True, the scaler object will be returned as well
+    '''
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    
+    scaler = MinMaxScaler()
+    scaler.fit(train[columns_to_scale])
+    
+    train_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
+                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+                                                  
+    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
+    
+    if return_scaler:
+        return scaler, train_scaled, validate_scaled, test_scaled
+    else:
+        return train_scaled, validate_scaled, test_scaled
+
 
 
 def wrangle_zillow():
@@ -75,49 +126,4 @@ def wrangle_zillow():
 
     return df
 
-### SPLIT #####
-def train_validate_test_split(df, target, seed=123):
-    '''
-    This function takes in a dataframe, the name of the target variable, and an integer for a setting a seed
-    and splits the data into train, validate and test.
-    Test is 20% of the original dataset, validate is .30*.80= 24% of the
-    original dataset, and train is .70*.80= 56% of the original dataset.
-    The function returns, in this order, train, validate and test dataframes.
-    '''
-    train_validate, test = train_test_split(df, test_size=0.2,
-                                            random_state=seed)
-    train, validate = train_test_split(train_validate, test_size=0.3,
-                                       random_state=seed)
-    return train, validate, test
 
-#### Scale #####
-def scale_data(train, 
-               validate, 
-               test, 
-               columns_to_scale=['bedroomcnt', 'bathroomcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet'],
-               return_scaler=False):
-    '''
-    Scales the 3 data splits. 
-    Takes in train, validate, and test data splits and returns their scaled counterparts.
-    If return_scalar is True, the scaler object will be returned as well
-    '''
-    train_scaled = train.copy()
-    validate_scaled = validate.copy()
-    test_scaled = test.copy()
-    
-    scaler = MinMaxScaler()
-    scaler.fit(train[columns_to_scale])
-    
-    train_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
-                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
-                                                  
-    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
-                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
-    
-    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
-                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
-    
-    if return_scaler:
-        return scaler, train_scaled, validate_scaled, test_scaled
-    else:
-        return train_scaled, validate_scaled, test_scaled
